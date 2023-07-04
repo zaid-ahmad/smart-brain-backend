5b1edd4069612ed570a4a9d05b7a1ec58c78d137
Zaid Ahmad <zaidd250@gmail.com>
2023-07-04 22:45:42 +0530

    add routes

--- index.js
+++ index.js
@@ -2,8 +2,100 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'apples',
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'john@gmail.com',
+    },
+  ],
+}
+
 app.get('/', (req, res) => {
-  res.send('this is working')
+  res.send(database.users)
+})
+
+app.post('/signin', (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json('success')
+  } else {
+    res.status(400).json('error logging in')
+  }
+})
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  })
+
+  res.json(database.users.at(-1))
+})
+
+app.get('/profile/:id', (req, res) => {
+  let found = false
+  const { id } = req.params
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true
+      return res.json(user)
+    }
+  })
+  if (!found) {
+    res.status(400).json('no such user')
+  }
+})
+
+app.put('/image', (req, res) => {
+  let found = false
+  const { id } = req.body
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true
+      user.entries++
+      return res.json(user.entries)
+    }
+  })
+  if (!found) {
+    res.status(400).json('no such user')
+  }
 })
 
 app.listen(3000, () => console.log('app is running on port 3000'))
+
+/*
+    / --> res = this is working
+    /signin --> POST = success/fail
+    /register --> POST = user
+    /profile/:userId --> GET = user
+    /image --> PUT --> user 
+ */
