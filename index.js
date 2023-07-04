1da0a9ddbb1b3f2330b92658106861d5a61fb364
Zaid Ahmad <zaidd250@gmail.com>
2023-07-04 22:09:58 +0530

    setup project

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('this is working')
+})
+
+app.listen(3000, () => console.log('app is running on port 3000'))
