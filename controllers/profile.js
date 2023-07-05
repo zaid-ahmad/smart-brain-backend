8af0374c9903a55df009b98e91b30398283a35c7
Zaid Ahmad <zaidd250@gmail.com>
2023-07-05 22:13:08 +0530

    add database, modifify for deployment

---
+++ controllers/profile.js
@@ -0,0 +1,17 @@
+const handleProfile = () => (req, res) => {
+  const { id } = req.params
+  db.select('*')
+    .from('users')
+    .where({
+      id,
+    })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('Not Found')
+      }
+    })
+}
+
+export default handleProfile
--- index.js
+++ index.js
@@ -1,94 +1,41 @@
 import express from 'express'
+import cors from 'cors'
+import knex from 'knex'
+import bcrypt from 'bcrypt-nodejs'
+
+/* CONTROLLERS */
+import handleRegister from './controllers/register.js'
+import handleSignin from './controllers/signin.js'
+import handleProfile from './controllers/profile.js'
+import handleImage from './controllers/image.js'
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    port: 5432,
+    user: 'postgres',
+    password: 'test',
+    database: 'smart-brain',
+  },
+})
 
 const app = express()
 
 app.use(express.json())
-
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'apples',
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-  login: [
-    {
-      id: '987',
-      hash: '',
-      email: 'john@gmail.com',
-    },
-  ],
-}
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send(database.users)
 })
 
-app.post('/signin', (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json('success')
-  } else {
-    res.status(400).json('error logging in')
-  }
-})
+app.post('/signin', handleSignin(db, bcrypt))
 
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date(),
-  })
+app.post('/register', handleRegister(db, bcrypt))
 
-  res.json(database.users.at(-1))
-})
+app.get('/profile/:id', handleProfile())
 
-app.get('/profile/:id', (req, res) => {
-  let found = false
-  const { id } = req.params
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true
-      return res.json(user)
-    }
-  })
-  if (!found) {
-    res.status(400).json('no such user')
-  }
-})
-
-app.put('/image', (req, res) => {
-  let found = false
-  const { id } = req.body
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true
-      user.entries++
-      return res.json(user.entries)
-    }
-  })
-  if (!found) {
-    res.status(400).json('no such user')
-  }
-})
+app.put('/image', handleImage(db))
 
 app.listen(3000, () => console.log('app is running on port 3000'))
 
