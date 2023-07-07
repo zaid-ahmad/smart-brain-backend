defda383e1f526f74996bc92a0b53e7cad64336f
Zaid Ahmad <zaidd250@gmail.com>
2023-07-07 22:53:58 +0530

    afkadfajdf

--- index.js
+++ index.js
@@ -26,9 +26,8 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(bodyParser.json())
 app.use(
   cors({
     origin: '*',
